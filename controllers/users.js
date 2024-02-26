0e93cc0dbd3a70c0a0c1ae493826321d84313249
kevy60 <kevin.vorsmann@voco.ee>
2024-02-26 14:10:27 +0200

    Fix existing user and password limitation errors

--- controllers/users.js
+++ controllers/users.js
@@ -4,25 +4,42 @@ const User = require('../models/user')
 
 const register = (req, res) => {
     console.log(req.body)
-    bcrypt.hash(req.body.password, 10, (error, cryptPassword) => {
-        User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: cryptPassword
+    
+    // Kontrollib, kas nimi on juba olemas 
+    User.findOne({ where: { username: req.body.username } })
+        .then(existingUser => {
+            if (existingUser) {
+                return res.status(400).json({ message: 'Username already exists' });
+            } else {
+                // Kontrollib, parooli pikkust
+                if (req.body.password.length < 8) {
+                    return res.status(400).json({ message: 'Password must be at least 8 characters long' });
+                }
+                bcrypt.hash(req.body.password, 10, (error, cryptPassword) => {
+                    User.create({
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: cryptPassword
+                    })
+                    .then((registered) => {
+                        req.session.user = {
+                            username: registered.username,
+                            user_id: registered.id,
+                        };
+                        console.log(req.session)
+                        res.json({
+                            message: 'New user is registered',
+                            user: registered,
+                            user_session: req.session.user
+                        })
+                    })
+                })
+            }
         })
-        .then((registered) => {
-            req.session.user = {
-                username: registered.username,
-                user_id: registered.id,
-            };
-            console.log(req.session)
-            res.json({
-                message: 'New user is registered',
-                user: registered,
-                user_session: req.session.user
-            })
-        })
-    })
+        .catch(error => {
+            console.error('Error:', error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        });
 }
 
-module.exports = {register}
+module.exports = { register }
